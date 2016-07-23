831fef2cae17e171e3eaa2c2fa89f971428e5021
Florian Tönjes <skatepainter@yahoo.de>
2016-07-23 15:45:30 +0200

    Added Project model.

--- src/main/java/com/floriantoenjes/instateam/model/Project.java
+++ src/main/java/com/floriantoenjes/instateam/model/Project.java
@@ -1,4 +1,68 @@
 package com.floriantoenjes.instateam.model;
 
+import javax.persistence.ManyToMany;
+import java.util.List;
+
 public class Project {
+    private int id;
+    private String name;
+    private String description;
+    private String status;
+
+    @ManyToMany
+    private List<Role> rolesNeeded;
+
+    @ManyToMany
+    private List<Collaborator> collaborators;
+
+    public Project() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Role> getRolesNeeded() {
+        return rolesNeeded;
+    }
+
+    public void setRolesNeeded(List<Role> rolesNeeded) {
+        this.rolesNeeded = rolesNeeded;
+    }
+
+    public List<Collaborator> getCollaborators() {
+        return collaborators;
+    }
+
+    public void setCollaborators(List<Collaborator> collaborators) {
+        this.collaborators = collaborators;
+    }
 }
